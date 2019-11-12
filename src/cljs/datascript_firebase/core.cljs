11a8a4c086531e82e0c95e1f4c2073f6df2c845b
Josh Brown <brown.joshua49@aol.com>
2019-11-12 02:17:18 -0500

    adds ui

--- src/cljs/datascript_firebase/core.cljs
+++ src/cljs/datascript_firebase/core.cljs
@@ -1,14 +1,42 @@
 (ns datascript-firebase.core
   (:require
-   [reagent.core :as r]))
+   [reagent.core :as r]
+   [datascript-firebase.reactive :as dr]
+   [datascript-firebase.firebase :as firebase]))
 
 
-(defn page []
-  [:div "hello world"])
+(defn page [{:as ctx :keys [conn transact!]}]
+  (r/with-let [*ints (dr/q
+                      '[:find [?int ...]
+                        :where
+                        [_ :test ?int]]
+                      conn)]
+    [:div
+     {:style {:margin-top   "24px"
+              :text-align "center"
+              }}
+     [:button
+      {:on-click #(transact! [{:db/id -1
+                               :test  (int (rand 1000))}])}
+      "Add integer"]
+     (into
+      [:div]
+      (doall
+       (for [i @*ints]
+         [:div i])))]))
+
+
+(defn page-wrapper []
+  (r/with-let [*ctx (firebase/create-ctx)]
+    (if @*ctx
+      [page @*ctx]
+      [:div
+       "loading..."])
+    ))
 
 
 (defn reload []
-  (r/render [page]
+  (r/render [page-wrapper]
             (.getElementById js/document "app")))
 
 
@@ -18,7 +46,7 @@
 
 (defn ^:export main []
   (enable-console-print!)
-  ;; (fire-core/init!)
+  (firebase/init!)
   (reload))
 
 
